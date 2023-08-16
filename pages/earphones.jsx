a7833071fc6c86a3e5a72d2c97b3a5215f0dd0fd
Rex <lubwamaemmanuel1@gmail.com>
2023-08-16 20:01:32 +0300

    feat:(added new pages)

---
+++ pages/earphones.jsx
@@ -0,0 +1,9 @@
+import PageHead from "../components/Page_Head/Page_Head";
+
+export default function Earphones() {
+  return (
+    <>
+      <PageHead title={"earphones"} />
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,10 @@ import Head from "next/head";
 import Hero from "../components/Hero/Hero";
 import Main from "../components/Main_homepage/Main";
 import Zx9 from "../components/Zx9/Zx9";
+import Zx7 from "../components/Zx7/Zx7";
+import Yx1 from "../components/Yx1/Yx1";
+import Gear from "../components/Gear/Gear";
+import Footer from "../components/Footer/Footer";
 
 export default function Homepage() {
   return (
@@ -12,6 +16,10 @@ export default function Homepage() {
       <Hero />
       <Main />
       <Zx9 />
+      <Zx7 />
+      <Yx1 />
+      <Gear />
+      <Footer />
     </>
   );
 }
